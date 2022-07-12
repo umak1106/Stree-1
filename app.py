e41f2ff7d9eafd2695014b1ed3905ba6e2c6d1ac
Uma Kadam <90750049+umak1106@users.noreply.github.com>
2022-07-12 10:27:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,9 @@ selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 page = PAGES[selection]
 page.app()
 st.sidebar.header('About Me & this project')
-st.sidebar.write('Hi! I am Uma kadam from IIIT Guwahati and Stree++ won under the category Best Healthtech Hack in Superposition VI. from'
-                 ' 523+ attendees from 23+ countries hosted on Devpost and EventBrite.')
+st.sidebar.write('Hi! I am Uma kadam from IIIT Guwahati.')
 
 st.sidebar.write("[Linkedin](https://www.linkedin.com/in/uma-kadam-7885341b0/)")
 st.sidebar.write("[Github](https://github.com/umak1106)")
-st.sidebar.write("[Devpost](https://devpost.com/software/stree)")
+
 
